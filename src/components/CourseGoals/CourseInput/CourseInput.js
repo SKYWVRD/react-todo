bd14f087773600056799a934b61cca0e34ac00c2
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-17 20:22:35 +0200

    Add dynamic styling for incorrect inputs

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,6 +5,7 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isInputValid, setIsInputValid] = useState(true);
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
@@ -12,14 +13,24 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsInputValid(false);
+      return
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isInputValid ? 'red' : 'black' }}>Course Goal</label>
+        <input type="text"
+          onChange={goalInputChangeHandler}
+          style={{
+            borderColor: !isInputValid ? 'red' : 'black',
+            backgroundColor: !isInputValid ? 'salmon' : 'white'
+          }}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
